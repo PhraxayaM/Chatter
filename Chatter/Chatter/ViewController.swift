dc147bcfbe3d937694caa307f8f73143b11ef247
PhraxayaM <mphraxa@gmail.com>
2020-07-25 11:07:57 -0700

    Initial commit. Removed storyboards. Xcode setup for programmatic views

---
+++ Chatter/Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by MattHew Phraxayavong on 7/25/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
